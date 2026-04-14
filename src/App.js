6be0d7baae1850cb0a54d99b215a074bc543a9ae
Victor Kovalchuk <vict02r05@gmail.com>
2026-04-14 18:41:59 +0300

    dispatching create customer and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import { useSelector } from "react-redux";
+
 import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
 
 function App() {
+  const customer = useSelector(store => store.customer.fullName);
+
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {!customer ? 
+        <CreateCustomer /> : 
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      }
     </div>
   );
 }
